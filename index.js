7c9cc27b3cf1cc2117be9cdf199fea01b3e3d9f3
pullso <p.dubitskiy@ya.ru>
2020-11-09 00:03:45 +0300

    fix helmet config

--- index.js
+++ index.js
@@ -55,7 +55,9 @@ app.use(session({
 app.use(fileMiddleware.single('avatar'))
 app.use(csrf())
 app.use(flash())
-app.use(helmet())
+app.use(helmet({
+  contentSecurityPolicy: false,
+}))
 app.use(compression())
 app.use(varMiddleware)
 app.use(userMiddleware)
